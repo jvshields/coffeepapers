1350f50a3db36337072f121fa8f88c36daf9bb0b
rodot- <jto33@drexel.edu>
2021-10-20 20:46:53 -0400

    made the interface more forgiving when given with incorrect args.  Now crashes if it does not have at least 2 args so it doesn't mess with your files if you forget them

--- query.py
+++ query.py
@@ -18,94 +18,92 @@ def cleanhtml(raw_html):
     cleanedmore = re.sub('\'', '’', cleanedmore)
     return(cleanedmore)
 
-def query(querytitle, share_date):
-    queryfixed = querytitle.replace(' ', '+')
-    queryfixed = queryfixed.replace('_', '+AND+ti:')
-    
-    base_url = 'http://export.arxiv.org/api/query?search_query=ti:'
-    q = base_url + queryfixed + '&start=0&max_results=1'
-    with libreq.urlopen(q) as url:
-      r = url.read()
-    
-    feed = feedparser.parse(r)
+def query(share_date, *querytitles):
 
-    data = feed.entries[0]
-    
-    uncleantitle = data.title
-    title = cleanhtml(uncleantitle)
-    
-    authorsunclean = (', '.join(author.name for author in data.authors))
-    authors = re.sub('\'', '’', authorsunclean)
-    
-    link = data.link
- 
-    for link2 in data.links:
-        if link2.rel == 'alternate':
-            pass
-        elif link2.title == 'pdf':
-            pdf = link2.href
-            
-    dictionary = {
-    'title'   : title,
-    'authors' : authors,
-    'url'  : link,
-    'pdf'  : pdf,
-    'date' : share_date # Date queried, not necessarily posted
-    }
-    
-    
-    return(dictionary)
-
-
-
-
-full_list = []
-
-# The last arg is the date it'll be shared.
-# Any format, just a string. "Tu Oct 12" etc is fine.
-# We have to pass the last arg as the date though, or it'll mess up.
-share_date = sys.argv[-1]
-
-for arg in sys.argv[1:-1]:
-
-    title = arg
-    query_result = query(title, share_date)
-    full_list.append(query_result)
-
-with open('papers.js', 'w') as outfile:
-    outfile.write('data=\'')
-    json.dump(full_list, outfile)
-    outfile.write('\'')
-    
-# The following loads and updates the archive json file
-# It's annoying and messy tbh. Python's JSON reader and the html one
-# are using different formats. I keep a file "old_papers.json" that is a 
-# cumulative list of all past and current papers. We load and dump to that, 
-# then use the updated data to prep "old_papers.js" for the html script. 
-# It works but isn't very clean.
-
-# Make sure to check "old_papers.json" exists. 
-# If not, dump full_list to it and move on
-if not os.path.isfile("old_papers.json"):
-    
-    data = full_list
-    with open('old_papers.json', 'w') as f:
-        json.dump(full_list, f)
-
-# If "old_papers.json" exists, load it, update its data will full_list
-# and dump.
-else:
-    with open('old_papers.json') as f:
-        data = json.load(f)
-
-        for i in range(len(full_list)):
-            data.insert(0, full_list[i])
+    for querytitle in querytitles:
+        queryfixed = querytitle.replace(' ', '+')
+        queryfixed = queryfixed.replace('_', '+AND+ti:')
         
-    with open('old_papers.json', 'w') as f:
-        json.dump(data, f)
+        base_url = 'http://export.arxiv.org/api/query?search_query=ti:'
+        q = base_url + queryfixed + '&start=0&max_results=1'
+        with libreq.urlopen(q) as url:
+          r = url.read()
+        
+        feed = feedparser.parse(r)
 
-# Finally, use "data" to create the ".js" file for the html script.
-with open('old_papers.js', 'w') as outfile:
-    outfile.write('data=\'')
-    json.dump(data, outfile)
-    outfile.write('\'')
+        data = feed.entries[0]
+        
+        uncleantitle = data.title
+        title = cleanhtml(uncleantitle)
+        
+        authorsunclean = (', '.join(author.name for author in data.authors))
+        authors = re.sub('\'', '’', authorsunclean)
+        
+        link = data.link
+     
+        for link2 in data.links:
+            if link2.rel == 'alternate':
+                pass
+            elif link2.title == 'pdf':
+                pdf = link2.href
+                
+        dictionary = {
+        'title'   : title,
+        'authors' : authors,
+        'url'  : link,
+        'pdf'  : pdf,
+        'date' : share_date # Date queried, not necessarily posted
+        }
+        
+        
+        yield dictionary
+
+
+
+if __name__ == '__main__':
+
+    # The last arg is the date it'll be shared.
+    # Any format, just a string. "Tu Oct 12" etc is fine.
+    # We have to pass the last arg as the date though, or it'll mess up.
+    *titles, share_date = sys.argv[1:]
+    assert titles, "Please provide 1 or more paper titles followed by a time string"
+    full_list = list(query(share_date, *titles))
+
+
+    with open('papers.js', 'w') as outfile:
+        outfile.write('data=\'')
+        json.dump(full_list, outfile)
+        outfile.write('\'')
+        
+    # The following loads and updates the archive json file
+    # It's annoying and messy tbh. Python's JSON reader and the html one
+    # are using different formats. I keep a file "old_papers.json" that is a 
+    # cumulative list of all past and current papers. We load and dump to that, 
+    # then use the updated data to prep "old_papers.js" for the html script. 
+    # It works but isn't very clean.
+
+    # Make sure to check "old_papers.json" exists. 
+    # If not, dump full_list to it and move on
+    if not os.path.isfile("old_papers.json"):
+        
+        data = full_list
+        with open('old_papers.json', 'w') as f:
+            json.dump(full_list, f)
+
+    # If "old_papers.json" exists, load it, update its data will full_list
+    # and dump.
+    else:
+        with open('old_papers.json') as f:
+            data = json.load(f)
+
+            for i in range(len(full_list)):
+                data.insert(0, full_list[i])
+            
+        with open('old_papers.json', 'w') as f:
+            json.dump(data, f)
+
+    # Finally, use "data" to create the ".js" file for the html script.
+    with open('old_papers.js', 'w') as outfile:
+        outfile.write('data=\'')
+        json.dump(data, outfile)
+        outfile.write('\'')
